ea483156e6a0fb4eef1ba14ecf989319508f220e
Jonatas <jmoraes@gmail.com>
2023-01-10 16:53:05 -0300

    Seed initial roles and groups

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,11 +6,63 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.user.upsert({
     where: { username: 'admin' },
-    update: {},
+    update: {
+      updatedAt: new Date(),
+    },
     create: {
       username: 'admin',
       password: await bcrypt.hash('admin', 10),
       createdAt: new Date(),
+      groups: {
+        create: [
+          {
+            group: {
+              create: {
+                name: 'Admin',
+                roles: {
+                  create: [
+                    {
+                      role: {
+                        create: {
+                          name: 'CREATE_ROLE',
+                          description: 'User can create new roles',
+                        },
+                      },
+                    },
+                    {
+                      role: {
+                        create: {
+                          name: 'CREATE_GROUP',
+                          description: 'User can create new groups',
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+            },
+          },
+          {
+            group: {
+              create: {
+                name: 'Manager',
+                roles: {
+                  create: [
+                    {
+                      role: {
+                        create: {
+                          name: 'ASSIGN_USERS',
+                          description: 'User can assign groups to users',
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        ],
+      },
     },
   });
 }
